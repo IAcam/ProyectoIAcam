c5ffd3477827cdcdd57a657d76f741376f713044
roheruton <roheru.ton@gmail.com>
2020-02-07 12:51:51 -0400

    se aÑaden algunas correcciones para las img de las plantillas

--- sw1/iacam/application/views/camara/visualizarCamara.php
+++ sw1/iacam/application/views/camara/visualizarCamara.php
@@ -1,13 +1,3 @@
-<style>
-   .picture-container {
-            position: relative;
-            width: 300;
-            height: 250px;
-            margin: 20px auto;
-            border: 10px solid #fff;
-            box-shadow: 0 5px 5px #000;
-        }
-</style>
 <div class="row">
     <div class="col-md-12">
         <div class="box box-info">
@@ -23,18 +13,19 @@
                             <label name="dirip" class="form-control" id="dirip"><?php echo ($this->input->post('dirip') ? $this->input->post('dirip') : $camara['dirip']); ?></label>
                         </div>
                     </div>
-
-                    <div class="col-sm-9">
-                      <iframe src="http://192.168.0.20"  width="720" height="640"></iframe>
-                    </div>
-
-                    <div class="col-sm-3">
-                        <div class="picture-container" id="imagen"> </div>
-                    </div>
                 </div>
             </div>
             <center>
-   
+            <div class="box-body">
+                <div class="row clearfix">
+                    <div class="col-md-20">
+                        <h1>integrar codigo aqui para acceder a camara</h1> 
+                        <video controls="play"></video>
+
+                    </div>
+                </div>
+            </div>
+
             
             <div class="box-footer">
                
@@ -45,4 +36,3 @@
         </div>
     </div>
 </div>
-<!-- <script src="../../../resources/js/watson.js"></script> -->
